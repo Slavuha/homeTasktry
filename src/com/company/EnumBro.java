7b0dbbf087f7a8a47bad3111b62226e9514c9b46
Mazur <Slavaslidchum@gmail.com>
2022-04-12 20:52:11 +0300

    Go

--- src/com/company/EnumBro.java
+++ src/com/company/EnumBro.java
@@ -1,9 +1,23 @@
 package com.company;
 
-public enum EnumBro {
-    ADMIN,
-    GUEST,
-    AUTORIZATE,
-    USER;
+public enum EnumBro implements Comparable <EnumBro> {
+    ADMIN(0),
+    GUEST(1),
+    AUTORIZATE(2),
+    USER(3);
 
+public int number;
+
+    EnumBro(int i) {
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
+
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,33 +7,49 @@ import java.util.TreeSet;
 public class Main {
 
     public static void main(String[] args) {
-        User user1 = new User("Vlad", "31", EnumBro.ADMIN);
-        User user2 = new User("Olha", "28", EnumBro.USER);
-        ArrayList arrayList = new ArrayList<>();
-        arrayList.add(user1);
-        arrayList.add(user2);
+        User user1 = new User("Vlad", 31, EnumBro.ADMIN, "111");
+        User user2 = new User("Olha", 28, EnumBro.USER, "222");
+        User user3 = new User("Masha", 32, EnumBro.GUEST, "333");
+        User user4 = new User("Aliona", 27, EnumBro.AUTORIZATE, "242");
+        User user5 = new User("Marina", 30, EnumBro.GUEST, "2242");
+        User user6 = new User("Dima", 21, EnumBro.USER, "222");
+        User user7 = new User("Petya", 22, EnumBro.USER, "222");
+        User user8 = new User("Vova", 23, EnumBro.USER, "222");
+        User user9 = new User("Vita", 24, EnumBro.USER, "222");
+        User user10 = new User("Oksana", 25, EnumBro.USER, "222");
+        ArrayList<User> arrayList = new ArrayList<>();
+        arrayList.add((user1));
+        arrayList.add((user2));
+        arrayList.add((user3));
+        arrayList.add((user4));
+        arrayList.add((user5));
+        arrayList.add((user6));
+        arrayList.add((user7));
+        arrayList.add((user8));
+        arrayList.add((user9));
+        arrayList.add((user10));
         System.out.println(arrayList.hashCode());
+        System.out.println(arrayList);
 
-        TreeSet<String> treeSet = new TreeSet<>(arrayList);
-
-   //     treeSet.add(user1);
-    //    treeSet.add(user2);
+        TreeSet<User> treeSet = new TreeSet<>(arrayList);
         System.out.println(treeSet.size());
-
+        System.out.println(treeSet);
 
 
     }
 }
-class User implements Comparable<User>  {
+class User implements Comparable<User> {
 
     String name;
-    String age;
+    int age;
+    String password;
     EnumBro enumBro;
 
-    public User(String name, String age, EnumBro enumBro) {
+    public User(String name, int age, EnumBro enumBro, String password) {
         this.name = name;
         this.age = age;
         this.enumBro = enumBro;
+        this.password = password;
     }
 
 
@@ -46,9 +62,25 @@ class User implements Comparable<User>  {
     }
 
     @Override
-    public int compareTo(User o) {
-        // how to right ))
-        return 0;
+    public int compareTo(User u) {
+      if (u.age > this.age) {
+          return -1;
+      }
+      if (u.age == this.age){
+          return 0;}
+          if (u.age < this.age){
+              return 1;
+      }
+          if (u.enumBro == this.enumBro) {
+                return 0;          }
+      /*  if (u.enumBro > this.enumBro) {
+            return 1;          }
+        if (u.enumBro < this.enumBro) {
+            return -1;          }
+*/
+        else return 0;
     }
+
+
 }
 
